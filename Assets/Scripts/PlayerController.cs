bbe69606edd8e4832ab7c46ae5b55dd3f658324d
GenoMcGahee <gmcgahee@student.fitchburgstate.edu>
2024-11-26 12:08:53 -0500

    project work 11/26

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    public GameObject arrowPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Instantiate(arrowPrefab, transform.position, arrowPrefab.transform.rotation);
+        }
+    }
+}
